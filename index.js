fbdb48437de7969cb6ab24f66e1197fd0f27693d
annacteixeira <20202003930@aluno.cefetmg.br>
2023-11-20 10:28:26 -0300

    resolução

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("Hello World")
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function calcularSaldoENivel(vitorias, derrotas) {
+    const saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) nivel = 'Ferro';
+    else if (vitorias <= 20) nivel = 'Bronze';
+    else if (vitorias <= 50) nivel = 'Prata';
+    else if (vitorias <= 80) nivel = 'Ouro';
+    else if (vitorias <= 90) nivel = 'Diamante';
+    else if (vitorias <= 100) nivel = 'Lendário';
+    else nivel = 'Imortal';
+
+    console.log(`O Herói tem saldo de ${saldoVitorias} está no nível de ${nivel}`);
+}
+
+function obterEntradaUsuario() {
+    rl.question('Deseja calcular o saldo e nível de rankeadas? (s/n): ', (resposta) => {
+        if (resposta.toLowerCase() === 's') {
+            rl.question('Digite a quantidade de vitórias: ', (vitorias) => {
+                rl.question('Digite a quantidade de derrotas: ', (derrotas) => {
+                    calcularSaldoENivel(parseInt(vitorias), parseInt(derrotas));
+                    obterEntradaUsuario();
+                });
+            });
+        } else {
+            rl.close();
+        }
+    });
+}
+
+obterEntradaUsuario();
